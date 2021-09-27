21f73f61f606c712e9d9da1d4b593e7688258418
submarine214 <72904986+submarine214@users.noreply.github.com>
2021-09-28 03:48:57 +0900

    Update WaterflowSensor_webOS.ino

--- HW/Automobile/Waterflow/WaterflowSensor_webOS.ino
+++ HW/Automobile/Waterflow/WaterflowSensor_webOS.ino
@@ -1,4 +1,3 @@
-
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
@@ -84,9 +83,37 @@ void loop(){
     
     WiFiClient client = server.available();   // listen for incoming clients
     if (client) {                             // if you get a client,
-    Serial.println("New Client.");           // print a message out the serial port
+      Serial.println("New Client.");           // print a message out the serial port
+      String currentLine = "";                // make a String to hold incoming data from the client
+
 
        while (client.connected()&& onoroff==1){
+        if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+          Serial.println("if_c_start");
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            Serial.println("if_length_start");
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+
+        
        onoroff = digitalRead(Switch); //Switch 값을 읽고 off state& reset 결정.
         currentMillis = millis();
         if (currentMillis - previousMillis > interval) {
@@ -144,9 +171,8 @@ void loop(){
                     Serial.println(httpResponseCode);
                     }
             }
-            }
-       }
-      }
+        }
+     }
     }
   }
 }
