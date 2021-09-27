19cbba85d940f7417cc6ed65bbae46fd4587bf64
MarcoBackman <roni2006@hanmail.net>
2021-09-27 10:01:25 -0400

    Update loginPage.js

--- WebOS/SmartHome/web_source/script/loginPage.js
+++ WebOS/SmartHome/web_source/script/loginPage.js
@@ -45,37 +45,16 @@ function browser_alert(message) {
   alert(message);
 }
 
-window.onload = function() {
-  const togglePassword = document.querySelector('#togglePassword');
-  const enter = document.querySelector('#login_button');
-  const user_pw = document.querySelector('#user_pw');
+ window.onload = function() {
+    const togglePassword = document.querySelector('#togglePassword');
+    const enter = document.querySelector('#login_button');
+    const user_pw = document.querySelector('#user_pw');
 
-  togglePassword.addEventListener('click', function (e) {
-  // toggle the type attribute
-  const type = user_pw.getAttribute('type') === 'password' ? 'text' : 'password';
-  user_pw.setAttribute('type', type);
-  // toggle the eye / eye slash icon
-  this.classList.toggle('bi-eye');
-});
-
-  /* //--for registeration or pw find
-  const form = document.querySelector("#signup");
-
-  const NAME_REQUIRED = "Please enter your name";
-  const EMAIL_REQUIRED = "Please enter your email";
-  const EMAIL_INVALID = "Please enter a correct email address format";
-
-  form.addEventListener("submit", function (event) {
-  	// stop form submission
-  	event.preventDefault();
-
-  	// validate the form
-  	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-  	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
-  	// if valid, submit the form.
-  	if (nameValid && emailValid) {
-  		alert("Demo only. No form was posted.");
-    }
+    togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = user_pw.getAttribute('type') === 'password' ? 'text' : 'password';
+    user_pw.setAttribute('type', type);
+    // toggle the eye / eye slash icon
+    this.classList.toggle('bi-eye');
   });
-  */
 }
